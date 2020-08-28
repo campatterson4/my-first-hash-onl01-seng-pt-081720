6983f3d05088da222b54f1ec497b2fbe12ac28d8
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-28 05:26:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,12 @@
 def my_hash
- cam = {"my name" => "Camryn", "my age"
+ cam = {"my name" => "Camryn", "my age" => "twenty one"}
 
   
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  
 
 
 end
